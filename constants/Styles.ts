f95f1c576fae6d4ded8e687d9fd5d3d94cbaa581
wargod <vishnuprasadvbhat@gmail.com>
2025-07-18 03:33:18 +0530

    mod: styles

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -1,4 +1,4 @@
-import Colors from '@/app-example/constants/Colors';
+import Colors from '@/constants/Colors';
 import { StyleSheet } from 'react-native';
 
 export const defaultStyles = StyleSheet.create({
