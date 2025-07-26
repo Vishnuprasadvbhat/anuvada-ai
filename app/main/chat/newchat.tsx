07a0ce1477fe3909dcb27a439ed20ac19384a389
wargod <vishnuprasadvbhat@gmail.com>
2025-07-26 17:18:47 +0530

    mod: new chat feat

--- app/main/chat/newchat.tsx
+++ app/main/chat/newchat.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const history = () => {
+const NewChat = () => {
   return (
     <View>
-      <Text>history</Text>
+      <Text>NewChat</Text>
     </View>
   )
 }
 
-export default history
+export default NewChat
